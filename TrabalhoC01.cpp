9e08c18b8769fd6e544e89152ce6df1dbca69927
Elias Alves <111067193+develiasx@users.noreply.github.com>
2022-10-07 23:34:12 -0300

    Update TrabalhoC01.cpp

--- TrabalhoC01.cpp
+++ TrabalhoC01.cpp
@@ -23,3 +23,45 @@ int main(void){
 	setlocale(LC_ALL, "Portuguese");
 	telaPrincipal();
 }
+//Função Cardapio 
+int fpedido(){
+	int option;
+	int ref;
+	printf("------------ CARDAPIO --------------\n");
+	printf("| 1 - Refeições | 2 - Tiragosto    |\n");
+	printf("| 3 - Bebidas   | 4 - Voltar       |\n");
+	printf("------------------------------------\n\n");
+	printf("Escolha a opção que você deseja fazer o pedido: ");
+	scanf("%d", &option);
+	system("cls");
+	switch (option){
+		case 1: {
+			printf("---------- CARDAPIO ----------\n");
+			printf("| 1 - Frango ao molho          |\n");
+			printf("-------------------------------\n");
+			printf("| 2 - Feijoada                 |\n");
+			printf("-------------------------------\n");
+			printf("| 3 - Panqueca                 |\n");
+			printf("-------------------------------\n");
+			printf("| 4 - Feijão Tropeiro          |\n");
+			printf("-------------------------------\n");
+			printf("Escolha uma Refeição: ");
+			scanf("%d", &ref);
+			printf("Informe a quantidade: ");
+			scanf("%d", &produto.quantidade);
+			if(ref == 1){
+				strcpy(produto.nome, "Frango ao molho");
+			}else if(ref == 2){
+				strcpy(produto.nome, "Feijoada");
+			}else if(ref == 3){
+				strcpy(produto.nome, "Panqueca");
+			}else if(ref == 4){
+				strcpy(produto.nome, "Feijão Tropeiro");
+			}else{
+				printf("Escolha uma opção válida!");
+				return fpedido();
+			}
+			printf("\nVocê escolheu o item: %s/quantidade: %d\n", produto.nome, produto.quantidade);
+			return fpedido();
+			break;
+		}
